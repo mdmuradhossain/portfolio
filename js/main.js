35aa3f46b3545ac0c4d9f0029903519f07c228af
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-10-14 22:26:13 +0600

    contact section implementing..

--- js/main.js
+++ js/main.js
@@ -22,69 +22,69 @@
 
 // Portfolio Popup
 
-(() => {
-  const filterContainer = document.querySelector(".portfolio-filter"),
-    portfolioItemsContainer = document.querySelector(".portfolio-items"),
-    portfolioItems = document.querySelectorAll(".portfolio-item"),
-    popup = document.querySelector(".portfolio-popup"),
-    prevBtn = popup.querySelector(".pp-prev"),
-    nextBtn = popup.querySelector(".pp-next"),
-    closeBtn = popup.querySelector(".pp-close"),
-    projectDetailsContainer = popup.querySelector(".pp-details"),
-    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+// (() => {
+//   const filterContainer = document.querySelector(".portfolio-filter"),
+//     portfolioItemsContainer = document.querySelector(".portfolio-items"),
+//     portfolioItems = document.querySelectorAll(".portfolio-item"),
+//     popup = document.querySelector(".portfolio-popup"),
+//     prevBtn = popup.querySelector(".pp-prev"),
+//     nextBtn = popup.querySelector(".pp-next"),
+//     closeBtn = popup.querySelector(".pp-close"),
+//     projectDetailsContainer = popup.querySelector(".pp-details"),
+//     projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
 
-  let itemIndex, slideIndex, screenshots;
+//   let itemIndex, slideIndex, screenshots;
 
-  filterContainer.addEventListener("click", (e) => {
-    if (
-      e.target.classList.contains("filter-item") &&
-      !e.target.classList.contains("active")
-    ) {
-      filterContainer
-        .querySelector(".active")
-        .classList.remove("outer-shadow", "active");
-      e.target.classList.add("active", "outer-shadow");
-      const target = e.target.getAttribute("data-target");
-    }
-  });
+//   filterContainer.addEventListener("click", (e) => {
+//     if (
+//       e.target.classList.contains("filter-item") &&
+//       !e.target.classList.contains("active")
+//     ) {
+//       filterContainer
+//         .querySelector(".active")
+//         .classList.remove("outer-shadow", "active");
+//       e.target.classList.add("active", "outer-shadow");
+//       const target = e.target.getAttribute("data-target");
+//     }
+//   });
 
-  portfolioItemsContainer.addEventListener("click", (e) => {
-    if (e.target.closest(".portfolio-item-inner")) {
-      const portfolioItem = e.target.closest(
-        ".portfolio-item-inner"
-      ).parentElement;
+//   portfolioItemsContainer.addEventListener("click", (e) => {
+//     if (e.target.closest(".portfolio-item-inner")) {
+//       const portfolioItem = e.target.closest(
+//         ".portfolio-item-inner"
+//       ).parentElement;
 
-      itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(
-        portfolioItem
-      );
-      console.log(itemIndex);
-      screenshots = portfolioItems[itemIndex]
-        .querySelector(".portfolio-item-img img")
-        .getAttribute("data-screenshots");
-      console.log(screenshots);
-      screenshots = screenshots.split(",");
-      slideIndex = 0;
-      popupToggle();
-      popupSlideshow();
-    }
-  });
+//       itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(
+//         portfolioItem
+//       );
+//       console.log(itemIndex);
+//       screenshots = portfolioItems[itemIndex]
+//         .querySelector(".portfolio-item-img img")
+//         .getAttribute("data-screenshots");
+//       console.log(screenshots);
+//       screenshots = screenshots.split(",");
+//       slideIndex = 0;
+//       popupToggle();
+//       popupSlideshow();
+//     }
+//   });
 
-  closeBtn.addEventListener("click", () => {
-    popupToggle();
-  });
-  function popupToggle() {
-    popup.classList.toggle("open");
-  }
-  function popupSlideshow() {
-    const imgSrc = screenshots[slideIndex];
-    const popupImg = popup.querySelector(".pp-img");
-    popup.querySelector(".pp-loader").classList.add("active");
-    popupImg.src = imgSrc;
+//   closeBtn.addEventListener("click", () => {
+//     popupToggle();
+//   });
+//   function popupToggle() {
+//     popup.classList.toggle("open");
+//   }
+//   function popupSlideshow() {
+//     const imgSrc = screenshots[slideIndex];
+//     const popupImg = popup.querySelector(".pp-img");
+//     popup.querySelector(".pp-loader").classList.add("active");
+//     popupImg.src = imgSrc;
 
-    popupImg.onload = () => {
-      popup.querySelector(".pp-loader").classList.remove("active");
-    };
-    popup.querySelector(".pp-counter").innerHTML =
-      slideIndex + 1 + " of " + screenshots.length;
-  }
-})();
+//     popupImg.onload = () => {
+//       popup.querySelector(".pp-loader").classList.remove("active");
+//     };
+//     popup.querySelector(".pp-counter").innerHTML =
+//       slideIndex + 1 + " of " + screenshots.length;
+//   }
+// })();
